8d129f6a10a18a358022978a152184023ef3d5d3
Yi Zhang <zhangyi1218@berkeley.edu>
2022-06-25 00:36:52 -0400

    finish kernel, blur for horizontal and vertical

--- BoxBlurFilter/src/main/scala/scalashop/package.scala
+++ BoxBlurFilter/src/main/scala/scalashop/package.scala
@@ -5,92 +5,103 @@ import org.scalameter._
 
 package object scalashop extends BoxBlurKernelInterface {
 
-  /** The value of every pixel is represented as a 32 bit integer. */
-  type RGBA = Int
+    /** The value of every pixel is represented as a 32 bit integer. */
+    type RGBA = Int
 
-  /** Returns the red component. */
-  def red(c: RGBA): Int = (0xff000000 & c) >>> 24
+    /** Returns the red component. */
+    def red(c: RGBA): Int = (0xff000000 & c) >>> 24
 
-  /** Returns the green component. */
-  def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
+    /** Returns the green component. */
+    def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
 
-  /** Returns the blue component. */
-  def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
+    /** Returns the blue component. */
+    def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
 
-  /** Returns the alpha component. */
-  def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
+    /** Returns the alpha component. */
+    def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
 
-  /** Used to create an RGBA value from separate components. */
-  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
-    (r << 24) | (g << 16) | (b << 8) | (a << 0)
-  }
+    /** Used to create an RGBA value from separate components. */
+    def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+        (r << 24) | (g << 16) | (b << 8) | (a << 0)
+    }
 
-  /** Restricts the integer into the specified range. */
-  def clamp(v: Int, min: Int, max: Int): Int = {
-    if (v < min) min
-    else if (v > max) max
-    else v
-  }
+    /** Restricts the integer into the specified range. */
+    def clamp(v: Int, min: Int, max: Int): Int = {
+        if (v < min) min
+        else if (v > max) max
+        else v
+    }
 
-  /** Image is a two-dimensional matrix of pixel values. */
-  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
-    def this(w: Int, h: Int) = this(w, h, new Array(w * h))
-    def apply(x: Int, y: Int): RGBA = data(y * width + x)
-    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
-  }
+    /** Image is a two-dimensional matrix of pixel values. */
+    class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+        def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+        def apply(x: Int, y: Int): RGBA = data(y * width + x)
+        def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
+    }
 
-  /** Computes the blurred RGBA value of a single pixel of the input image. */
-  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    /** Computes the blurred RGBA value of a single pixel of the input image. */
+    def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+        var count: Int = 0
+        var (r, g, b, a) = (0, 0, 0, 0)
+        for (i <- x-radius to x+radius) {
+            for (j <- y-radius to y + radius) {
+                if (this.clamp(i, 0, src.width-1) == i && this.clamp(j, 0, src.height-1) == j) {
+                    count = 1 + count
+                    r = r + this.red(src.apply(i, j))
+                    g = g + this.green(src.apply(i, j))
+                    b = b + this.blue(src.apply(i, j))
+                    a = a + this.alpha(src.apply(i, j))
+                }
+            }
+        }
+        this.rgba(r/count, g/count, b/count, a/count)
+    }
 
-    // TODO implement using while loops
-    ???
-  }
+    val forkJoinPool = new ForkJoinPool
 
-  val forkJoinPool = new ForkJoinPool
+    abstract class TaskScheduler {
+        def schedule[T](body: => T): ForkJoinTask[T]
+        def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
+            val right = task {
+                taskB
+            }
+            val left = taskA
+            (left, right.join())
+        }
+    }
 
-  abstract class TaskScheduler {
-    def schedule[T](body: => T): ForkJoinTask[T]
+    class DefaultTaskScheduler extends TaskScheduler {
+        def schedule[T](body: => T): ForkJoinTask[T] = {
+            val t = new RecursiveTask[T] {
+                def compute = body
+            }
+            Thread.currentThread match {
+                case wt: ForkJoinWorkerThread =>
+                    t.fork()
+                case _ =>
+                    forkJoinPool.execute(t)
+            }
+            t
+        }
+    }
+
+    val scheduler =
+        new DynamicVariable[TaskScheduler](new DefaultTaskScheduler)
+
+    def task[T](body: => T): ForkJoinTask[T] = {
+        scheduler.value.schedule(body)
+    }
+
     def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
-      val right = task {
-        taskB
-      }
-      val left = taskA
-      (left, right.join())
+        scheduler.value.parallel(taskA, taskB)
     }
-  }
 
-  class DefaultTaskScheduler extends TaskScheduler {
-    def schedule[T](body: => T): ForkJoinTask[T] = {
-      val t = new RecursiveTask[T] {
-        def compute = body
-      }
-      Thread.currentThread match {
-        case wt: ForkJoinWorkerThread =>
-          t.fork()
-        case _ =>
-          forkJoinPool.execute(t)
-      }
-      t
+    def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
+        val ta = task { taskA }
+        val tb = task { taskB }
+        val tc = task { taskC }
+        val td = taskD
+        (ta.join(), tb.join(), tc.join(), td)
     }
-  }
-
-  val scheduler =
-    new DynamicVariable[TaskScheduler](new DefaultTaskScheduler)
-
-  def task[T](body: => T): ForkJoinTask[T] = {
-    scheduler.value.schedule(body)
-  }
-
-  def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
-    scheduler.value.parallel(taskA, taskB)
-  }
-
-  def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
-    val ta = task { taskA }
-    val tb = task { taskB }
-    val tc = task { taskC }
-    val td = taskD
-    (ta.join(), tb.join(), tc.join(), td)
-  }
 
 }
